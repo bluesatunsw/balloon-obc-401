9d6953153e5eb301731498e8aa33ae843d05ea8c
Evan La Fontaine <navelafontaine@gmail.com>
2024-02-19 01:22:43 +1100

    [IMPR] Add Clangd support

--- utils/system-project.cmake
+++ utils/system-project.cmake
@@ -1,21 +1,20 @@
-macro(system_kernel kern_name sources includes link_script cpu_flags)
+macro(system_kernel kern_name sources includes link_script cpu_flags compile_flags)
     message(${CMAKE_CXX_COMPILER})
     if ($ENV{BUILD_PASS} STREQUAL "arm")
         add_executable(${kern_name} ${COMMON_SOURCES} ${sources})
         target_include_directories(${kern_name} PRIVATE
             "${COMMON_INCLUDES};${includes}"
         )
-        message("${COMMON_INCLUDES};${includes}")
         target_compile_options(
             ${kern_name} PUBLIC
-            ${COMMON_COMPILE_FLAGS} ${cpu_flags}
+            ${COMMON_COMPILE_FLAGS} ${cpu_flags} ${compile_flags}
             -mthumb -ffunction-sections -fdata-sections -fno-common
         )
         target_link_options(${kern_name} PUBLIC
             -T${link_script} ${cpu_flags}
             -Wl,-Map=${kern_name}.map
             --specs=nosys.specs
-            -gc-sections
+            -Wl,--gc-sections
             -u _printf_float
             -Wl,--start-group
             -lc -lm
