79e7f0a3bf81a63cf52f9843d65b472c5c6b8942
Bean Can <140042296+YellowChemistryPublishing@users.noreply.github.com>
2024-06-05 23:48:00 +1000

    Run clang-tidy in lint.

---
+++ utils/add-checks.cmake
@@ -0,0 +1,28 @@
+function(add_linter_target arg1 arg2)
+    set(TARGET_NAME ${arg1})
+
+    if(NOT TARGET ${TARGET_NAME})
+        message(FATAL_ERROR "Target ${TARGET_NAME} not found, can't be checked.")
+    endif()
+
+    get_target_property(TARGET_INCLUDE_DIRS ${TARGET_NAME} INCLUDE_DIRECTORIES)
+    get_target_property(TARGET_INTERFACE_INCLUDE_DIRS ${TARGET_NAME} INTERFACE_INCLUDE_DIRECTORIES)
+    if (TARGET_INCLUDE_DIRS)
+        list(APPEND TARGET_ALL_INCLUDE_DIRS ${TARGET_INCLUDE_DIRS})
+    endif()
+    if (TARGET_INTERFACE_INCLUDE_DIRS)
+        list(APPEND TARGET_ALL_INCLUDE_DIRS ${TARGET_INTERFACE_INCLUDE_DIRS})
+    endif()
+
+    get_target_property(TARGET_COMPILE_DEFS ${TARGET_NAME} COMPILE_DEFINITIONS)
+    if (NOT TARGET_COMPILE_DEFS)
+        set(TARGET_COMPILE_DEFS "")
+    endif()
+
+    add_custom_target(check-${TARGET_NAME}
+        ALL
+        # Yes, quote whole argument, otherwise list semicolons are omitted by CMake.
+        ${CMAKE_COMMAND} "-DCHECK_SRCS=${arg2}" "-DCHECK_INCLUDE_DIRS=${TARGET_ALL_INCLUDE_DIRS}" "-DCHECK_DEFINITIONS=${TARGET_COMPILE_DEFS}" -P "${CMAKE_SOURCE_DIR}/utils/run-checks.cmake"
+        WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+    )
+endfunction()
